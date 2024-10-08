5b45e5bc9a6599f74962bd28b48c725eaea4bcce
JonadelCabang <160558946+JonadelCabang@users.noreply.github.com>
2024-10-08 21:25:24 +0700

    Add files via upload

---
+++ quiz_one.py
@@ -0,0 +1,31 @@
+#Groupmates fullname 
+GroupmatesName1="Francene Gian B. Pais"
+GroupmatesName2="Abegail A. Daproza"
+GroupmatesName3="Jonadel N. Cabang"
+
+#Groupmates Age
+GroupmatesAge1="20"
+GroupmatesAge2="23"
+GroupmatesAge3="22"
+
+#Groupmates Allowance
+GroupmatesAllowance1="1,000"
+GroupmatesAllowance2="500"
+GroupmatesAllowance3="2,000"
+
+#TeamName Node js
+TeamNames="Node js"
+
+#Prints all Groupmates informations 
+print("Team Name: " + TeamNames)
+print("Mem1: " + GroupmatesName1 + ", her age is " + GroupmatesAge1+ ", Allowance per week is " + GroupmatesAllowance1)
+print("Mem2: " + GroupmatesName2 + ", her age is " + GroupmatesAge2+ ", Allowance per week is " + GroupmatesAllowance2)
+print("Mem3: " + GroupmatesName3 + ", her age is " + GroupmatesAge3+ ", Allowance per week is " + GroupmatesAllowance3)
+
+#SHows the Length of our Names
+Mem1NameLength = (len(GroupmatesName1))
+Mem2NameLength = (len(GroupmatesName2))
+Mem3NameLength = (len(GroupmatesName3))
+print(Mem1NameLength)
+print(Mem2NameLength)
+print(Mem3NameLength)
---
+++ quiz_two.py
@@ -0,0 +1,34 @@
+#CABANG, JONADEL N. QUIZ ONE
+#info of the teamLollipop group
+
+GroupMates1= "Francene Gian B. Pais"
+GroupMates2= "Abegail A. Daproza"
+GroupMates3= "Jonadel N. Cabang"
+
+#Declared the oge of teamLollipop group
+GroupmatesAge1="20"
+GroupmatesAge2="23"
+GroupmatesAge3="22"
+
+#The allowance of each member of Lollipop
+GroupmatesAllowance1="1,000"
+GroupmatesAllowance2="500"
+GroupmatesAllowance3="2,000"
+
+#Team Name
+TeamName= "Lollipop"
+
+#the length of the names of each member
+GroupMateName1Length1= (len (GroupMates1))
+GroupMateName2Length2= (len (GroupMates2))
+GroupMateName3Length3= (len (GroupMates3))
+
+#Total of Ages and Allowances
+mem1AgeAllowaneTotal1=(GroupmatesAge1+GroupmatesAllowance1)
+mem2AgeAllowaneTotal2=(GroupmatesAge2+GroupmatesAllowance2)
+mem3AgeAllowaneTotal3=(GroupmatesAge3+GroupmatesAllowance3)
+
+#The final Total of the Name,Age, and Allowance of each member of Lollipop mem
+print ("Group1: + GroupmatesAge1 + Allowance1")
+print ("Group2: + GroupmatesAge2 + Allowance2")
+print ("Group3: + GroupmatesAge3 + Allowance3")
